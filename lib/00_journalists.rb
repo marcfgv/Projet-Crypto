998b1792ae7d1e24519206d299b97f3d149fc470
marcfgv <114512442+marcfgv@users.noreply.github.com>
2022-10-06 23:43:46 +0200

    Add files via upload

---
+++ lib/00_journalists.rb
@@ -0,0 +1,65 @@
+a = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def 
+    puts "Total profils : #{a.count}\n\n"
+end
+
+def
+    puts a.sort_by(&:length)[0]
+end
+
+def
+    puts a.find_all {|b| b.size == 6}
+
+    b = a.find_all {|b| b.size == 6}
+    puts "#{b.count}\n\n"
+endcd 
+
+def
+    puts a.grep(/[UPPERCASE]/).length
+end
+
+def
+    puts a.sort
+end
+
+def 
+    puts a.sort_by!(&:length)
+end
+
+def 
+    puts a.index("@epenser")
+end
+
+def 
+    puts " Il y a #{a.count{|x| x.size==4}} handles avec 4 caractères"
+    puts " Il y a #{a.count{|x| x.size==5}} handles avec 5 caractères"
+    puts " Il y a #{a.count{|x| x.size==6}} handles avec 6 caractères"
+    puts " Il y a #{a.count{|x| x.size==7}} handles avec 7 caractères"
+    puts " Il y a #{a.count{|x| x.size==8}} handles avec 8 caractères"
+    puts " Il y a #{a.count{|x| x.size==9}} handles avec 9 caractères"
+    puts " Il y a #{a.count{|x| x.size==10}} handles avec 10 caractères"
+    puts " Il y a #{a.count{|x| x.size==11}} handles avec 11 caractères"
+    puts " Il y a #{a.count{|x| x.size==12}} handles avec 12 caractères"
+    puts " Il y a #{a.count{|x| x.size==13}} handles avec 13 caractères"
+    puts " Il y a #{a.count{|x| x.size==14}} handles avec 14 caractères"
+    puts " Il y a #{a.count{|x| x.size==15}} handles avec 15 caractères"
+    puts " Il y a #{a.count{|x| x.size==16}} handles avec 16 caractères"
+    puts " Il y a #{a.count{|x| x.size==17}} handles avec 17 caractères"
+    puts " Il y a #{a.count{|x| x.size==18}} handles avec 18 caractères"
+    puts " Il y a #{a.count{|x| x.size==19}} handles avec 19 caractères"
+    puts " Il y a #{a.count{|x| x.size==20}} handles avec 20 caractères"
+    puts " Il y a #{a.count{|x| x.size==21}} handles avec 21 caractères"
+    puts " Il y a #{a.count{|x| x.size==22}} handles avec 22 caractères"
+    puts " Il y a #{a.count{|x| x.size==23}} handles avec 23 caractères"
+    puts " Il y a #{a.count{|x| x.size==24}} handles avec 24 caractères"
+    puts " Il y a #{a.count{|x| x.size==25}} handles avec 25 caractères"
+    puts " Il y a #{a.count{|x| x.size==26}} handles avec 26 caractères"
+    puts " Il y a #{a.count{|x| x.size==27}} handles avec 27 caractères"
+    puts " Il y a #{a.count{|x| x.size==28}} handles avec 28 caractères"
+    puts " Il y a #{a.count{|x| x.size==29}} handles avec 29 caractères"
+    puts " Il y a #{a.count{|x| x.size==30}} handles avec 30 caractères"
+    puts " Il y a #{a.count{|x| x.size==31}} handles avec 31 caractères"
+    puts " Il y a #{a.count{|x| x.size==32}} handles avec 32 caractères"
+    puts " Il y a #{a.count{|x| x.size==33}} handles avec 33 caractères"
+end
